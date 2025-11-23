7a6db91bffe3dbe07140aff83cab24b73f2d96eb
anupamboral <anupamboral6889@gmail.com>
2025-11-23 14:38:53 +0530

    created project, installed daisy ui ,tailwind, created navbar

---
+++ allLessonNotes.js
@@ -0,0 +1,7 @@
+//! ⁢Season 2 - Episode - 15 - DevtinderUi Part-1
+//* we created this project using command -  npm create vite@latest dev-connect-frontend -- --template react
+//* to design our ui we will use tailwind css and diasy ui(a component library compatible with daisy ui.)
+//* then installed tailwind seeing it's doc.
+//* only after installing we can daisy as it's is interconnected , so let's go to daisy ui 's website and install daisy ui.using the command :-npm i -D daisyui@latest and in the css file we have to add:- @plugin "daisyui";
+//* in the left side navigation of daisy ui we will find navbar inside the components , so we will go inside the navbar and and see there is a "Navbar with search input and dropdown" so we will copy the jsx code of that (not html),and paste that inside the app.jsx file. and we will see a that in the ui the navbar is added into our ui.
+//* to change colors we can go to left side nav bar and go inside colors and all color guide in there
