47d3086754145c14b0a897d3a49b1545c7b9a6a0
anupamboral <anupamboral6889@gmail.com>
2025-12-14 14:33:32 +0530

    backend url updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://dev-connect-backend-sand.vercel.app";
+//* for test using localhost change the url to   "http://localhost:3000";
