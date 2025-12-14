b5657e5abeaa4b54b05f2ecd581990dc07a06829
anupamboral <anupamboral6889@gmail.com>
2025-12-14 14:51:01 +0530

    env variables added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,1 @@
-export const BASE_URL = "https://dev-connect-backend-sand.vercel.app";
-//* for test using localhost change the url to   "http://localhost:3000";
+export const BASE_URL = import.meta.env.BASE_URL;
